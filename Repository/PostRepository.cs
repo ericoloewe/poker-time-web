0f8c634838a9d87eea7e97d9560bea0226f3a39d
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-04-04 20:21:13 -0300

    refactor(ignores): add to git ignores

--- Repository/PostRepository.cs
+++ Repository/PostRepository.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using Domain;
 
 namespace Repository
 {
     public class PostRepository : IPostRepository
     {
+        public IList<Post> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
