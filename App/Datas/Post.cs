f814182918163b2622136e2138106a16c6d257d2
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-04-04 23:23:08 -0300

    feat(posts): add controller

---
+++ App/Datas/Post.cs
@@ -0,0 +1,9 @@
+namespace App.Datas
+{
+    public class PostData
+    {
+        public string Id { get; set; }
+        public string Image { get; set; }
+        public string Message { get; set; }
+    }
+}
